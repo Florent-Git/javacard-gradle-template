fdcbf953d2f6b444ab5e549847384c3fefa0ed35
Florent <florent.ram@gmail.com>
2023-02-10 10:20:13 +0100

    Javacard build successfull

---
+++ src/main/java/be/mgx/javacard/HelloWorld.java
@@ -0,0 +1,16 @@
+package be.mgx.javacard;
+
+import javacard.framework.APDU;
+import javacard.framework.Applet;
+import javacard.framework.ISOException;
+
+public class HelloWorld extends Applet {
+    @Override
+    public void process(APDU apdu) throws ISOException {
+
+    }
+
+    public static void install(byte[] var0, short var1, byte var2) throws ISOException {
+
+    }
+}
